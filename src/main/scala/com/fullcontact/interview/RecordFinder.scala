a291781741b5b5519809b370559f3ce0d00c7224
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2020-10-14 18:10:18 -0600

    added files

---
+++ src/main/scala/com/fullcontact/interview/RecordFinder.scala
@@ -0,0 +1,36 @@
+package com.fullcontact.interview
+import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.io.Source
+
+
+object RecordFinder {
+  def main(args: Array[String]): Unit = {
+
+
+    val conf = new SparkConf()
+      .setMaster("local")
+      .setAppName("RecordFinder")
+
+    val sc = new SparkContext(conf)
+
+
+    //create 2 RDD's filled with data from files
+    val queries_rdd = sc.textFile("./Queries.txt")
+    val records_rdd = sc.textFile("./Records.txt")
+    val empty_rdd = sc.emptyRDD[String]
+    val joined_rdd = queries_rdd.union(records_rdd)
+
+
+    println("##Get data Using collect")
+    joined_rdd.collect().foreach(f=>{
+      println(f)
+    })
+
+
+
+
+   /* val queries = Source.fromFile("./Queries.txt").getLines.toArray
+    val records = Source.fromFile("./Records.txt").getLines.toArray*/
+  }
+}//main
